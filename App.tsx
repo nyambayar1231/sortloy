fd1ef881f5aeb66d92c3f9717d782ed091faa835
Nyambayar1231 <nyambayarlucky@gmail.com>
2024-02-27 22:13:59 +0800

    initial commit

--- App.tsx
+++ App.tsx
@@ -1,10 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import * as Linking from 'expo-linking';
 
 export default function App() {
+  const apiUrl = process.env.EXPO_PUBLIC_API_URL || '';
+
+  function handleButtonPress() {
+    Linking.openURL(apiUrl);
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Hello world</Text>
+      <Button title="Click me" onPress={handleButtonPress} />
       <StatusBar style="auto" />
     </View>
   );
