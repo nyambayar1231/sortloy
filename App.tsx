8f731a5bd6ebf1fb74a689d98666102f50245bb2
Nyambayar1231 <nyambayarlucky@gmail.com>
2024-02-27 23:07:22 +0800

    BUILD FIRST DISPLAY USING ASSETS AND @EXPO/VECTOR LIBRARY

--- App.tsx
+++ App.tsx
@@ -1,18 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button } from 'react-native';
-import * as Linking from 'expo-linking';
+import { StyleSheet, View } from 'react-native';
+
+import Button from './components/Button';
+import ImageViewer from './components/ImageViewer';
+
+const PlaceholderImage = require('./assets/images/background-image.png');
 
 export default function App() {
-  const apiUrl = process.env.EXPO_PUBLIC_API_URL || '';
-
-  function handleButtonPress() {
-    Linking.openURL(apiUrl);
-  }
-
   return (
     <View style={styles.container}>
-      <Text>Hello world</Text>
-      <Button title="Click me" onPress={handleButtonPress} />
+      <View style={styles.imageContainer}>
+        <ImageViewer placeholderImageSource={PlaceholderImage} />
+      </View>
+      <View style={styles.footerContainer}>
+        <Button label="Choose a photo" theme="primary" />
+        <Button label="Use this photo" />
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -21,8 +24,15 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#25292e',
     alignItems: 'center',
-    justifyContent: 'center',
+  },
+  imageContainer: {
+    flex: 1,
+    paddingTop: 58,
+  },
+  footerContainer: {
+    flex: 1 / 3,
+    alignItems: 'center',
   },
 });
